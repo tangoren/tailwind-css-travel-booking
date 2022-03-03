7abf86ff676588bc3ebd6be5622e65ef4891e959
Osman Tangoren <otangoren@gmail.com>
2022-03-03 15:32:40 +0300

    Container sizes updated, custom fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
    content: ["./src/**/*.{html,js}"],
    theme: {
-      extend: {},
+      screens: {
+         sm: "480px",
+         md: "768px",
+         lg: "976px",
+         xl: "1440px",
+      },
+      container: {
+         center: true,
+      },
+      extend: {
+         fontFamily: {
+            sans: ["Inter", "sans-serif"],
+         },
+      },
    },
    plugins: [],
 };
