f14396e4db20e31818414c6ae8ea5ab9a9a3a38d
Osman Tangoren <otangoren@gmail.com>
2022-03-04 16:40:04 +0300

    Tour content header added, mobile menu toggle icon fix

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const body = document.querySelector("body");
 const toggleButton = document.querySelector(".menu-toggle");
+let toggleContent = toggleButton.querySelector("span");
 
 window.addEventListener("resize", handeResize);
 toggleButton.addEventListener("click", handleMenu);
@@ -8,12 +9,12 @@ function handeResize() {
    window.outerWidth <= 992 && body.classList.contains("menu-open")
       ? body.classList.add("menu-open")
       : body.classList.remove("menu-open");
+
+   toggleContent.textContent = "menu";
 }
 
 function handleMenu() {
    body.classList.toggle("menu-open");
-
-   let toggleContent = toggleButton.querySelector("span");
 
    body.classList.contains("menu-open")
       ? (toggleContent.textContent = "close")
